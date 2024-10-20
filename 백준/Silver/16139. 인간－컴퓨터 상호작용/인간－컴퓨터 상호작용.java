08a29963b36c9daa0ba33c8dac19545ad51aa783
TaeyongJang <163623205+Taeyong98@users.noreply.github.com>
2024-10-20 15:50:51 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 728 ms, Memory: 103204 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,32 +1,49 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int n = Integer.parseInt(br.readLine());
-        int[][] alphabet = new int[str.length()+1][26];
-        for(int i = 1; i <= str.length(); i++){
-            for(int j = 0; j<26;j++){
-                if(j == str.charAt(i-1)-'a'){
-                    alphabet[i][j] = alphabet[i-1][j] +1;
-                }
-                else {
-                    alphabet[i][j] = alphabet[i-1][j];
-                }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+
+        int length = input.length();
+        int[][] psum = new int[26][length + 1];
+
+        fillCounts(psum, input);
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+
+            String query = br.readLine();
+            printCount(bw, psum, query);
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    public static void fillCounts(int[][] psum, String input) {
+        int[] cnt = new int[26];
+
+        for (int i = 0; i < input.length(); i++) {
+            int ch = input.charAt(i) - 'a';
+            cnt[ch]++;
+            for (int j = 0; j < 26; j++) {
+                psum[j][i + 1] = cnt[j];
             }
-
         }
-        for(int i = 0; i<n;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            char c = st.nextToken().charAt(0);
-            int firstIndex = Integer.parseInt(st.nextToken());
-            int lastIndex = Integer.parseInt(st.nextToken());
-            System.out.println(alphabet[lastIndex+1][c-'a'] - alphabet[firstIndex][c-'a']);
-        }
+    }
 
+    public static void printCount(BufferedWriter bw, int[][] psum, String query) throws IOException {
+        StringTokenizer st = new StringTokenizer(query);
+        int ch = st.nextToken().charAt(0) - 'a';
+        int from = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+        bw.write(psum[ch][to + 1] - psum[ch][from] + "\n");
     }
 }
